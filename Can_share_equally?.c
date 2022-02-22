3280caef76d6731344160bfef01055075d32cbd8
moulilalam <99000887+moulilalam@users.noreply.github.com>
2022-02-22 21:53:14 +0530

    #include<stdio.h> int main() {     int X,Y,a;     scanf("%d%d",&X,&Y);     a=X+2*Y;     if(a%2==0)     {         if(X%2==0 && Y%2==0)         {             printf("YES");         }         else if(X==0 && Y%2!=0)         {             printf("NO");         }         else if(X%2==0 && 2*Y%2==0)         {             printf("YES");         }     }     else     {         printf("NO");     } }

---
+++ Can_share_equally?.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int X,Y,a; 
+    scanf("%d%d",&X,&Y);
+    a=X+2*Y;
+    if(a%2==0)
+    {
+        if(X%2==0 && Y%2==0)
+        {
+            printf("YES");
+        }
+        else if(X==0 && Y%2!=0)
+        {
+            printf("NO");
+        }
+        else if(X%2==0 && 2*Y%2==0)
+        {
+            printf("YES");
+        }
+    }
+    else
+    {
+        printf("NO");
+    }
+}
+       
