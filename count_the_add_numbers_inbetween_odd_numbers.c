e8a77df9a51f8140e97c8274755160453caa5837
moulilalam <99000887+moulilalam@users.noreply.github.com>
2022-05-04 20:30:40 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             if(arr[i-1]%2==1 && arr[i+1]%2==1)             {                 c++;             }         }     }     printf("%d",c); }

---
+++ count_the_add_numbers_inbetween_odd_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100],c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            if(arr[i-1]%2==1 && arr[i+1]%2==1)
+            {
+                c++;
+            }
+        }
+    }
+    printf("%d",c);
+}
