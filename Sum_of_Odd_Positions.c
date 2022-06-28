d6cf90615a3d6006e2be045b8e4783ee9df6da90
moulilalam <99000887+moulilalam@users.noreply.github.com>
2022-06-28 20:00:27 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,sum=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(i%2!=0)         {             sum=sum+arr[i];         }     }     printf("%d",sum); }

--- Sum_of_Odd_Positions.c
+++ Sum_of_Odd_Positions.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,arr[100],sum=0;
+    int n,arr[100],i,sum=0;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
@@ -9,7 +9,7 @@ int main()
     }
     for(i=0;i<n;i++)
     {
-        if(i%2==1)
+        if(i%2!=0)
         {
             sum=sum+arr[i];
         }
