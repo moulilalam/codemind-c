586b9382e46809e824cfeb563f7d4e44d42a16e6
moulilalam <99000887+moulilalam@users.noreply.github.com>
2022-06-29 21:43:07 +0530

    #include<stdio.h> int main() {     int r,c,a[100][100],i,j,s=0,k=0;     scanf("%d%d",&r,&c);     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             k=k+a[i][j];         }     }     for(i=1;i<r-1;i++)     {         for(j=1;j<c-1;j++)         {             s=s+a[i][j];         }     }     printf("%d",k-s);
    }

---
+++ Sum_of_all_boundary_values_in_a_matrix.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int r,c,a[100][100],i,j,s=0,k=0;
+    scanf("%d%d",&r,&c);
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            k=k+a[i][j];
+        }
+    }
+    for(i=1;i<r-1;i++)
+    {
+        for(j=1;j<c-1;j++)
+        {
+            s=s+a[i][j];
+        }
+    }
+    printf("%d",k-s);
+    
+}
