9a3b67db1861f26a8b3676d0cc75450baacb8bda
moulilalam <99000887+moulilalam@users.noreply.github.com>
2022-06-29 20:35:52 +0530

    #include<stdio.h> int main() {     int arr[200],n,i,k,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d",&k);     for(i=0;i<=n;i++)     {         if(k==arr[i])         {             c++;         }     }     printf("%d",c); }

--- count_of_given_element_in_an_array.c
+++ count_of_given_element_in_an_array.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,arr[100],z,c=0;
+    int arr[200],n,i,k,c=0;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-    scanf("%d",&z);
-    for(i=1;i<=n;i++)
+    scanf("%d",&k);
+    for(i=0;i<=n;i++)
     {
-        if(z==arr[i])
+        if(k==arr[i])
         {
-    //        printf("%d",i);
-            c=c+1;
+            c++;
         }
     }
     printf("%d",c);
