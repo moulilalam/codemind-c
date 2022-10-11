0b422a140a34a879c4e2c6283a689312513ec44b
moulilalam <99000887+moulilalam@users.noreply.github.com>
2022-10-11 11:34:18 +0530

    #include<stdio.h> int main() {     int n,m;     scanf("%d%d",&n,&m);     int a[n][m],i,j,s=0;     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<n;i++)     {         for(j=0;j<m;j++)         {             if(i!=0 and i!=n-1 and j!=0 and j!=m-1)             s+=a[i][j];         }     }     printf("%d",s); }

--- Sum_of_non_boundary_values_in_a_matrix.c
+++ Sum_of_non_boundary_values_in_a_matrix.c
@@ -1,22 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    int r,c,a[100][100],i,j,s=0;
-    scanf("%d%d",&r,&c);
-    for(i=0;i<r;i++)
+    int n,m;
+    scanf("%d%d",&n,&m);
+    int a[n][m],i,j,s=0;
+    for(i=0;i<n;i++)
     {
-        for(j=0;j<c;j++)
+        for(j=0;j<m;j++)
         {
             scanf("%d",&a[i][j]);
         }
     }
-    for(i=1;i<r-1;i++)
+    for(i=0;i<n;i++)
     {
-        for(j=1;j<c-1;j++)
+        for(j=0;j<m;j++)
         {
-            s=s+a[i][j];
+            if(i!=0 and i!=n-1 and j!=0 and j!=m-1)
+            s+=a[i][j];
         }
     }
     printf("%d",s);
-    
 }
